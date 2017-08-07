8b3f6bdc2472b5927d53aebeefcf06de6b9418ec
Esaú García Sánchez-Torija <2669347+egasato@users.noreply.github.com>
2017-08-07 17:05:43 +0200

    Updated indentation and removed useless PDF files.

--- patch.py
+++ patch.py
@@ -1,43 +1,67 @@
-# import re to compile regexp
+# -*- coding: utf-8 -*-
+# importamos "re" para compilar expresiones regulares
+# import "re" to compile regular expressions
 import re
 
-# import os to rename files
+# importamos "os" para renombrar archivos y manejar rutas
+# import "os" to rename files and handle paths
 import os
 
-# import ConfigParser to edit platformio.ini
+# importamos "ConfigParser" para editar archivos .ini
+# import "ConfigParser" to edit .ini files
 import ConfigParser
 
-# import serial.tools.list_ports to get all connected ports
+# importamos "serial.tools.list_ports" para obtener todos los puertos conectados
+# import "serial.tools.list_ports" to get all connected ports
 import serial.tools.list_ports
 
+# importamos "sys" para poder terminar el programa
+# import "sys" to terminate the program
+import sys
+
+# obtenemos una lista con todos los puertos
 # get a list of all ports
 ports = list(serial.tools.list_ports.comports())
-port  = None
+port = None
 
-# find the one that contains the word Arduino
+# buscamos el que contiene la palabra Arduino (si existe)
+# find the one that contains the word Arduino (if any)
 for p in ports:
     if "Arduino" in p[1]:
         port = p[0]
         break
 
-# print the first one if none was found
-if ports and not port:
-    port = ports[0][0]
+# si no encontramos ninguno usamos el primero, sino terminamos el programa
+# if we cannot find any use the first one, otherwise we terminate the program
+if not port:
+    if ports:
+        port = ports[0][0]
+    else:
+        print "No hay ningún puerto conectado"
+        print "No ports connected"
+        sys.exit()
 
+# creamos el modificador de configuración, leemos el archivo y lo actualizamos
 # start the configuration parser, parse the file and update it
 config = ConfigParser.RawConfigParser()
 config.read("platformio.ini")
 config.set("env:uno", "target_upload", port)
 
+# escribimos los cambios en el disco
 # write changes to disk
 with open("platformio.ini", "wb") as configfile:
     config.write(configfile)
 
+# leemos el archivo de nuevo y lo convertimos en una lista de lineas
 # read the file again into a list of lines
 with open("platformio.ini") as configfile:
     content = configfile.readlines()
 
-# compile the regex to detect whitespace-only lines and search from the end the first line that is not whitespace
+# compilamos la expresión regular para detectar lineas que solo contengan
+# carácteres no visibles y buscamos empezando por el final la primera linea que
+# que no coincide con esa expresión regular
+# compile the regex to detect whitespace-only lines and search from the end the
+# first line that does not match
 p = re.compile("^[\s \n\r]*$")
 i = 0
 for e in reversed(content):
@@ -45,12 +69,14 @@ for e in reversed(content):
         break
     i += 1
 
-# write everything until that one line
+# escribimos el archivo sin modificar hasta la línea detectada anteriormente
+# write everything but the everything after the line detected above
 with open("platformio.ini.new", "w") as output:
     output.write("".join(content[:-i]))
 
-# rename files and remove files
+# eliminamos el archivo viejo, renombramos el actual a .old y el nuevo como el actual
+# remove old files, rename the current to .old and the new as the current
 if os.path.exists("platformio.ini.old"):
     os.unlink("platformio.ini.old")
-os.rename("platformio.ini",     "platformio.ini.old")
+os.rename("platformio.ini", "platformio.ini.old")
 os.rename("platformio.ini.new", "platformio.ini")
--- src/main.cpp
+++ src/main.cpp
@@ -10,36 +10,36 @@ int switchState = 0;
 // presionado el pulsador de reset
 // Setup function executed once when the Arduino board is powered or the reset button is pressed
 void setup() {
-    // Definimos los LEDs para que funcionen en modo salida
-    // Set the LEDs to work in output mode
-    pinMode(5, OUTPUT);
-    pinMode(4, OUTPUT);
-    pinMode(3, OUTPUT);
-    // Definimos el pulsador para que funcione en modo entrada
-    // Set the button to work in input mode
-    pinMode(2, INPUT);
+	// Definimos los LEDs para que funcionen en modo salida
+	// Set the LEDs to work in output mode
+	pinMode(5, OUTPUT);
+	pinMode(4, OUTPUT);
+	pinMode(3, OUTPUT);
+	// Definimos el pulsador para que funcione en modo entrada
+	// Set the button to work in input mode
+	pinMode(2, INPUT);
 }
 
 // El código de Arduino que se ejecuta infinitamente (llamada implicita a la función loop al final)
 // Arduino code that executes forever (implicit call to the loop function at the end)
 void loop() {
-    // Leemos el valor de entrada del pulsador
-    // Read the input value of the button
-    switchState = digitalRead(2);
-    // Si no esta apretado solo se enciende el LED verde, si lo está se alternan los LEDs rojos una única vez
-    // If it's not pressed turn on only the green LED, otherwise blink the red ones alternately
-    if (switchState == LOW) {
-        digitalWrite(5, LOW);
-        digitalWrite(4, LOW);
-        digitalWrite(3, HIGH);
-    } else {
-        digitalWrite(5, HIGH);
-        digitalWrite(4, LOW);
-        digitalWrite(3, LOW);
-        delay(250);
-        digitalWrite(5, LOW);
-        digitalWrite(4, HIGH);
-        digitalWrite(3, LOW);
-        delay(250);
-    }
+	// Leemos el valor de entrada del pulsador
+	// Read the input value of the button
+	switchState = digitalRead(2);
+	// Si no esta apretado solo se enciende el LED verde, si lo está se alternan los LEDs rojos una única vez
+	// If it's not pressed turn on only the green LED, otherwise blink the red ones alternately
+	if (switchState == LOW) {
+		digitalWrite(5, LOW);
+		digitalWrite(4, LOW);
+		digitalWrite(3, HIGH);
+	} else {
+		digitalWrite(5, HIGH);
+		digitalWrite(4, LOW);
+		digitalWrite(3, LOW);
+		delay(250);
+		digitalWrite(5, LOW);
+		digitalWrite(4, HIGH);
+		digitalWrite(3, LOW);
+		delay(250);
+	}
 }
