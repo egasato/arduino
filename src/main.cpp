7ae14d829665559f967f3a9a07b073485fe6495a
Esaú García Sánchez-Torija <2669347+egasato@users.noreply.github.com>
2017-08-07 16:58:17 +0200

    Updated indentation and removed useless pages from the PDF guide.

--- src/main.cpp
+++ src/main.cpp
@@ -6,18 +6,18 @@
 // presionado el botón de reset
 // Setup function executed once when the Arduino board is powered or the reset button is pressed
 void setup() {
-    pinMode(LED_BUILTIN, OUTPUT);
+	pinMode(LED_BUILTIN, OUTPUT);
 }
 
 // El código de Arduino que se ejecuta infinitamente (llamada implicita a la función loop al final)
 // Arduino code that executes forever (implicit call to the loop function at the end)
 void loop() {
-    // Enciende el LED incorporado y espera 1 segundo
-    // Turns on the built-in LED and waits 1 second
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(1000);
-    // Apaga el LED incorporado y espera 1 segundo
-    // Turns off the built-in LED and waits 1 second
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(1000);
+	// Enciende el LED incorporado y espera 1 segundo
+	// Turns on the built-in LED and waits 1 second
+	digitalWrite(LED_BUILTIN, HIGH);
+	delay(1000);
+	// Apaga el LED incorporado y espera 1 segundo
+	// Turns off the built-in LED and waits 1 second
+	digitalWrite(LED_BUILTIN, LOW);
+	delay(1000);
 }
